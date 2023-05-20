ae5d39aba591679be688c7eb38823687dde9c6da
ValentinWuerz <valentinwuerz@web.de>
2023-05-20 17:03:54 +0200

    three huge progress, raycasting, objects, hdr, selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,149 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Raycaster, Vector2 } from 'three';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'first_steps_three';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('canvasContainer', { static: true }) canvasContainer!: ElementRef;
+
+  private renderer!: THREE.WebGLRenderer;
+  private scene!: THREE.Scene;
+  private camera!: THREE.PerspectiveCamera;
+  private controls!: OrbitControls;
+  private raycaster!: THREE.Raycaster;
+  private mouse!: THREE.Vector2;
+  private selectedObject: THREE.Object3D | null = null;;
+  private priorcolor: any;
+  ngOnInit(): void {
+    // Create your Three.js scene and objects here
+    this.scene = new THREE.Scene();
+    this.scene.background = null;
+    this.raycaster = new THREE.Raycaster();
+    this.mouse = new THREE.Vector2();
+        // Load HDR texture
+        const loader = new RGBELoader();
+        loader.load('assets/textures/pine_attic_4k.hdr', (texture) => {
+          texture.mapping = THREE.EquirectangularReflectionMapping;
+          this.scene.environment = texture;
+          this.scene.background = texture
+                // Create a directional light
+        const light = new THREE.DirectionalLight(0xffffff, 1);
+        light.position.set(1, 1, 1);
+        //this.scene.add(light);
+
+              // Create an ambient light
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+      //this.scene.add(ambientLight);
+        });
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshPhysicalMaterial({
+          color: 0xB2B2DD,
+          envMap: this.scene.environment, // Set the environment texture as the envMap
+          roughness: 0,
+          metalness: 1
+        });
+        const cube = new THREE.Mesh(geometry, material);
+        //this.scene.add(cube);
+        const gltfLoader = new GLTFLoader();
+        gltfLoader.load('assets/models/cubes.glb', (gltf) => {
+          const object = gltf.scene;
+  
+          this.scene.add(object);
+        });
+  }
+
+  ngAfterViewInit(): void {
+    // Create and configure the renderer
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.canvasContainer.nativeElement.appendChild(this.renderer.domElement);
+    this.renderer.domElement.addEventListener('mousemove', this.onMouseMove.bind(this), false);
+
+    // Create the camera
+    const aspect = window.innerWidth / window.innerHeight;
+    this.camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+    this.camera.position.z = 5;
+
+    // Create and configure the OrbitControls
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.05;
+
+    // Start rendering the scene
+    this.render();
+  }
+  private onMouseMove(event: MouseEvent): void {
+    //change color of all object that are not selected to 0x6FA8DC
+    if ( this.selectedObject ) {
+
+      this.selectedObject.traverse((child: any) => {
+        if (child instanceof THREE.Mesh) {
+          // Change the color of the intersected object
+          child.material.color.set(this.priorcolor); // Set the color to red
+          //child.scale.set(1.0, 1.0, 1.0); // Set the color to red
+          //child.scale.multiplyScalar(0.8);
+        }
+      });
+      this.selectedObject = null;
+      this.priorcolor=null
+    }
+    event.preventDefault();
+  
+    // Calculate normalized device coordinates (NDC) based on mouse position
+    const canvas = this.renderer.domElement;
+    const rect = canvas.getBoundingClientRect();
+    this.mouse.x = ((event.clientX - rect.left) / canvas.clientWidth) * 2 - 1;
+    this.mouse.y = -((event.clientY - rect.top) / canvas.clientHeight) * 2 + 1;
+  this.raycaster.setFromCamera(this.mouse, this.camera);
+
+  }
+  
+  private render(): void {
+      // Update the raycaster with the current mouse position
+
+  // Perform the raycasting and get the intersected objects
+  const intersects = this.raycaster.intersectObjects(this.scene.children, true);
+
+
+  // Handle the intersected objects
+  if (intersects.length > 0) {
+    const res = intersects.filter( function ( res ) {
+
+      return res && res.object;
+
+    } )[ 0 ];
+    if ( res && res.object ) {
+
+      this.selectedObject = res.object;
+      this.selectedObject.traverse((child: any) => {
+        if (child instanceof THREE.Mesh) {
+          if(!this.priorcolor){
+          this.priorcolor=child.material.color.clone();
+          console.log(this.priorcolor);
+          }
+          // Change the color of the intersected object
+          child.material.color.set(0xff0000);
+          //child.scale.set(1.2, 1.2, 1.2); // Set the color to red
+          //child.scale.multiplyScalar(1.2);
+        }
+      });
+
+    }
+
+    // For example, change the color of the intersected object
+  }
+  // Render the scene
+  this.renderer.render(this.scene, this.camera);
+
+  // Request the next frame
+  requestAnimationFrame(() => this.render());
+    this.controls.update();
+    this.renderer.render(this.scene, this.camera);
+  }
 }
